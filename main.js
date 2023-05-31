c37df10fbff9b091c6f5f1e53fd13aecc45d3657
Adil Khamitov <100089827+Admjral@users.noreply.github.com>
2023-05-31 19:54:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let userName = prompt("Введите свое имя");
+let userAge = prompt("Введите свой возраст")
+let userSurname = prompt("Введите свою фамилию")
+let profession = prompt("Введите профессию")
+document.write("Добро пожаловать " + userName);
+document.write("Ваше имя: " + userName);
+document.write("Возраст: " + userAge);
+document.write("Ваша Фамилия: " + userSurname);
+document.write("Профессия: " + profession);
